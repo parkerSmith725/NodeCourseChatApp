37bf412b951b0b8ca9da9bdd33daa16f01f17b25
Parker <parker.smith725@gmail.com>
2018-03-16 16:06:03 -0600

    Emit new message on create message

--- server/server.js
+++ server/server.js
@@ -14,6 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
     console.log('New user connected');
 
+    socket.on('createMessage',(message) => {
+        console.log('Received message',message);
+        io.emit('newMessage', {
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
+        });
+    });
+
     socket.on('disconnect',() => {
         console.log('Disconnected from client');
     });
