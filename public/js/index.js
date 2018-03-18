a34e69321ab0577db8668ca0a6aa207bc84c8ba1
Parker <parker.smith725@gmail.com>
2018-03-17 20:52:18 -0600

    Add mustache js

--- public/js/index.js
+++ public/js/index.js
@@ -10,22 +10,26 @@ socket.on('disconnect',function() {
 
 socket.on('newMessage',function(message){
     var time = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    li.text(`${message.from} ${time}: ${message.text}`);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template,{
+        text:message.text,
+        from:message.from,
+        createdAt:time
+    });
 
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage',function(message){
     var time = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My Current Location</>');
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template,{
+        url:message.url,
+        from:message.from,
+        createdAt:time
+    });
 
-    li.text(`${message.from} ${time}: `);
-    a.attr('href',message.url);
-
-    li.append(a);
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 var messageTextBox = $('[name=message]');
